160fbd05bc2f1f5d075d15b635626915aba7f30f
Donny Bereznak <dbereznak@gmail.com>
2024-12-13 20:04:08 -0500

    added Header and Footer

---
+++ email-template-viewer/src/app/components/footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./footer.module.css"
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2>&copy; Donny Bereznak</h2>
+        </footer>
+    );
+}
---
+++ email-template-viewer/src/app/components/header.tsx
@@ -0,0 +1,9 @@
+import styles from "./header.module.css"
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <h2>Email Template Viewer</h2>
+        </header>
+    );
+}
