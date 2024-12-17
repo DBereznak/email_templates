ab43a300c215f5bf80d424571cd75daa920e48e6
Donny Bereznak <dbereznak@gmail.com>
2024-12-17 11:26:59 -0500

    added code highlighting

--- email-template-viewer/src/app/components/EmailHTML.tsx
+++ email-template-viewer/src/app/components/EmailHTML.tsx
@@ -1,9 +1,13 @@
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { darcula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { RawEmailProp } from "./appTypes";
 
 export default function EmailHTML({rawHTML}: RawEmailProp) {
     return (
         <div>
+            <SyntaxHighlighter language="HTML" style={darcula}>
             {rawHTML}
+            </SyntaxHighlighter>
         </div>
     )
 }
