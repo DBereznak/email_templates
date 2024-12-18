933954722420d6047fcee8d7e4e40112bb60645f
Donny Bereznak <dbereznak@gmail.com>
2024-12-17 21:06:31 -0500

    started adding state

--- email-template-viewer/src/app/components/emailTemplates.ts
+++ email-template-viewer/src/app/components/emailTemplates.ts
@@ -1,3 +1,9 @@
-export const emailTemplates = [
-    {id: 1, email: "<table><tr><th>Title</th><tr><tr><td>This is a test template</td><tr></table>"}
-];
+
+export default function emailMarkup(name: string) {
+    return [
+        {id: 1, email: "<table><tr><th>Title</th><tr><tr><td>This is a test template</td><tr></table>"},
+        {id: 2, email: `<table><tr><th>Title</th><tr><tr><td>This is a second test template</td><tr><tr><td>${name} is Great</td><tr></table>`},
+        {id: 3, email: `<table><tr><th>Title</th><tr><tr><td>This is a third test template</td><tr><tr><td>${name} is Awesome</td><tr></table>`},
+    ];
+    
+}
